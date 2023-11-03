943028f33838d84b9ec3c8bcf3afdca8518c73f6
AGgit <ag.git1111@gmail.com>
2023-11-03 13:42:01 -0600

    Added notes to JS for better understanding.

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ const displayDiv = document.getElementById('displayContent');
 // Add an event listener to the list for the click event
 list.addEventListener('click', function (event) {
     // Check if the clicked event is a list item
+    // 'event.target' refers to the element that was clicked.
+    // 'nodeName' property is used to check if the clicked element is a <li> element.
     if (event.target && event.target.nodeName === "LI") {
+        // The '||' is the OR operator used to return the first operand if truthy and if not, it will return the second operand.
         const content = event.target.textContent || event.target.innerText;
 
         displayDiv.textContent = content;
